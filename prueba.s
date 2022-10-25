f95e8232e1b03ae4ca410225612cff06c2e93285
ImportFromCBastian <sebshndz2001@gmail.com>
2022-10-25 13:49:40 -0300

    Added a assembly file

---
+++ prueba.s
@@ -0,0 +1,12 @@
+.data
+    peso: .double 83,7
+    estatura: .double 1,83
+    infra: .double 18,5
+    norm: .double 25,0
+    sobr: .double 30,0
+    imc: .double 0
+    estado: .double 0
+.code
+    ld r1 , A(r2)
+    sd r2 , B(r0) 
+halt
